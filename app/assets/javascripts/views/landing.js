75a615ffb41d019e5bfd77f6e889aa0ad4b8a22a
Judy Long <long.judy92@gmail.com>
2016-06-28 18:22:19 -0400

    show/hide question

--- app/assets/javascripts/views/landing.js
+++ app/assets/javascripts/views/landing.js
@@ -8,6 +8,7 @@ MentalMath.Views.Landing = Backbone.View.extend({
     }
     this.expOptions = "";
     this.right = 11;
+    this.showText = false;
   },
 
   template: JST['landing/landing'],
@@ -25,19 +26,38 @@ MentalMath.Views.Landing = Backbone.View.extend({
   },
 
   renderCard: function() {
-    var exp = this.makeExpression();
+    if (!this.currentExp) {
+      this.currentExp = this.makeExpression();
+    }
 
-    var utterance = new SpeechSynthesisUtterance(exp.speech);
+    var utterance = new SpeechSynthesisUtterance(this.currentExp.speech);
     utterance.lang = 'en-US';
     window.speechSynthesis.speak(utterance);
 
-    var question = {'text': exp.text};
+    var question;// = {'text': this.currentExp.text};
+    if (this.showText) {
+      question = {'text': this.currentExp.text};
+    } else {
+      question = "";
+    }
     $('.card').html(JST['landing/card']({question: question}));
     return this;
   },
 
   events: {
-    "keyup .answer": "submitAnswer"
+    "keyup .answer": "submitAnswer",
+    "click .showText": "setShowText"
+  },
+
+  setShowText: function(e) {
+    // e.preventDefault();
+    debugger
+    if (e.currentTarget.checked) {
+      this.showText = true;
+    } else {
+      this.showText = false;
+    }
+    this.renderCard();
   },
 
   submitAnswer: function(e) {
@@ -45,6 +65,7 @@ MentalMath.Views.Landing = Backbone.View.extend({
     if (e.keyCode === 13) {
       if (this.checkingSolution(e.target.value)) {
         $('.correct').show();
+        this.currentExp = this.makeExpression();
         setTimeout(this.renderCard.bind(this),1000);
       } else {
         $('.wrong').show();
