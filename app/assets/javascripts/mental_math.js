42da5cfe2bb15673395ee96427feef970a40c281
Judy Long <long.judy92@gmail.com>
2016-06-28 15:56:14 -0400

    instantiate header

--- app/assets/javascripts/mental_math.js
+++ app/assets/javascripts/mental_math.js
@@ -4,6 +4,7 @@ window.MentalMath = {
   Views: {},
   Routers: {},
   initialize: function() {
+    this.header = new MentalMath.Views.Header({el: $('#header')});
     this.router = new MentalMath.Routers.Router({$rootEl: $('#main')});
     Backbone.history.start();
   }
