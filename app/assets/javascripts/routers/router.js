43eed2b926e8b9833d9d8d86bf964b432e90a76f
Judy Long <long.judy92@gmail.com>
2016-07-01 19:55:44 -0400

    update backbone router for signing up and logging in

--- app/assets/javascripts/routers/router.js
+++ app/assets/javascripts/routers/router.js
@@ -5,6 +5,8 @@ MentalMath.Routers.Router = Backbone.Router.extend({
 
   routes: {
     "":"landing",
+    "users/new": "newUser",
+    "session/new": "logIn"
   },
 
   landing: function() {
@@ -12,9 +14,44 @@ MentalMath.Routers.Router = Backbone.Router.extend({
     this._swapView(view);
   },
 
+  newUser: function() {
+    if (!this._requireLoggedOut()) {return;}
+    var model = new MentalMath.Models.User();
+    var view = new MentalMath.Views.UsersSignup({model: model});
+    this._swapView(view);
+  },
+
+  logIn: function(callback) {
+    if (!this._requireLoggedOut(callback)) {return;}
+    var view = new MentalMath.Views.Login({callback: callback});
+    this._swapView(view);
+  },
+
   _swapView: function(view) {
     this._currentView && this._currentView.remove();
     this._currentView = view;
     this.$rootEl.html(this._currentView.render().$el);
+  },
+
+  _requireLoggedIn: function(callback) {
+    if (!MentalMath.currentUser.isLoggedIn()) {
+      callback = callback || this._goHome.bind(this);
+      this.logIn(callback);
+      return false;
+    }
+    return true;
+  },
+
+  _requireLoggedOut: function(callback) {
+    if (MentalMath.currentUser.isLoggedIn()) {
+      callback = callback || this._goHome.bind(this);
+      callback();
+      return false;
+    }
+    return true;
+  },
+
+  _goHome: function() {
+    Backbone.history.navigate("", {trigger: true});
   }
-})
+});
