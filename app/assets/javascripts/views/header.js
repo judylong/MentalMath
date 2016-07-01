b0654732c7043869c17c787e88ce5c27e442a5e3
Judy Long <long.judy92@gmail.com>
2016-07-01 19:51:51 -0400

    update header to render buttons for login/signup/signout based on currentUser state

--- app/assets/javascripts/views/header.js
+++ app/assets/javascripts/views/header.js
@@ -1,10 +1,26 @@
 MentalMath.Views.Header = Backbone.View.extend({
   initialize: function() {
+    this.listenTo(MentalMath.currentUser, "logIn logOut", this.render);
     this.render();
   },
+
+  events: {
+    "click #log-out-link": "logOut"
+  },
+
   template: JST['shared/header'],
+
   render: function() {
-    this.$el.html(this.template());
+    this.$el.html(this.template({currentUser: MentalMath.currentUser}));
     return this;
+  },
+
+  logOut: function(e) {
+    e.preventDefault();
+    MentalMath.currentUser.logOut({
+      success: function() {
+        Backbone.history.navigate("session/new", {trigger: true});
+      }
+    });
   }
 });
