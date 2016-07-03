cea9c09f1d315942052a45cc1e51ad707eda1258
Judy Long <long.judy92@gmail.com>
2016-07-02 20:30:35 -0400

    update routes for recordings

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   namespace :api, defaults: {format: :json} do
     resources :users, only: [:create, :show]
     resource :session, only: [:create, :show, :destroy]
+    resources :recordings, only: [:create]
   end
 end
