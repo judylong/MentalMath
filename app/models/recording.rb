e65c40baf31a7c8c8f631d19aed2eddf8b1e72a5
Judy Long <long.judy92@gmail.com>
2016-07-03 23:55:23 -0400

    update recording model and users controller

--- app/models/recording.rb
+++ app/models/recording.rb
@@ -2,4 +2,7 @@ class Recording < ActiveRecord::Base
   validates :level, :duration_ms, :user_id, presence: true
   validates_inclusion_of :correct, :in => [true, false]
   belongs_to :user
+
+  scope :leveln, -> (level, bool) {where(level: level, correct: bool)}
+
 end
